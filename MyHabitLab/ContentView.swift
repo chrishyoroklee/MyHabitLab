88818ba5fd813b4ad87028bab5e56e951cc1ed20
Chris Hyorok Lee <hl3838@columbia.edu>
2026-01-02 14:05:54 -0500

    Initial Commit

---
+++ MyHabitLab/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyHabitLab
+//
+//  Created by 이효록 on 1/2/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyHabitLab/MyHabitLabApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyHabitLabApp.swift
+//  MyHabitLab
+//
+//  Created by 이효록 on 1/2/26.
+//
+
+import SwiftUI
+
+@main
+struct MyHabitLabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
